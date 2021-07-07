4fbf3b6c71ddff08b570d2fcda2a0e64cae3e01f
Corentin Clichy <cor.clichy@gmail.com>
2021-07-07 09:04:28 -0400

    change eslintConfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,13 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['airbnb-base'],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
+  extends: ['airbnb', 'prettier', 'plugin:node/recommended'],
+  plugins: ['prettier'],
   rules: {
-    'no-underscore-dangle': 'off',
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'func-names': 'off',
+    'no-process-exit': 'off',
+    'object-shorthand': 'off',
+    'class-methods-use-this': 'off',
   },
 };
